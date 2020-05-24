06957571c7dfdad0b8bb5d05c6b01f4eb44982d4
ReinaldoDomingos <reijunior100@gmail.com>
2020-05-24 18:12:36 -0400

    Adicionado service Lancamento  e teste unitário

---
+++ src/main/java/com/reinaldo/pontointeligente/api/services/LancamentoService.java
@@ -0,0 +1,44 @@
+package com.reinaldo.pontointeligente.api.services;
+
+import com.reinaldo.pontointeligente.api.entities.Funcionario;
+import com.reinaldo.pontointeligente.api.entities.Lancamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Optional;
+
+public interface LancamentoService {
+    /**
+     * Retorna uma lista de lançamentos de um determinado funcionario.
+     *
+     * @param funcionarioId
+     * @param pageRequest
+     * @return Page<Lancamento>
+     */
+    Page<Lancamento> buscarPorFunionarioId(Long funcionarioId, PageRequest pageRequest);
+
+    /**
+     * Retorna um lançamento por ID.
+     *
+     * @param id
+     * @return Optional<Lancamento>
+     */
+    Optional<Lancamento> buscarPorId(Long id);
+
+
+    /**
+     * Persiste um lancamento na base de dados
+     *
+     * @param lancamento
+     * @return Lancamento
+     */
+    Lancamento persistir(Lancamento lancamento);
+
+    /**
+     * Remove um lancamento na base de dados.
+     *
+     * @param id
+     */
+    void remover(Long id);
+
+}
---
+++ src/main/java/com/reinaldo/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -0,0 +1,46 @@
+package com.reinaldo.pontointeligente.api.services.impl;
+
+
+import com.reinaldo.pontointeligente.api.entities.Lancamento;
+import com.reinaldo.pontointeligente.api.repositories.LancamentoRepository;
+import com.reinaldo.pontointeligente.api.services.LancamentoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+    private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    @Override
+    public Page<Lancamento> buscarPorFunionarioId(Long funcionarioId, PageRequest pageRequest) {
+        log.info("\nBuscando lancamentos para o funcionario ID {}\n", funcionarioId);
+        return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+    }
+
+    @Override
+    public Optional<Lancamento> buscarPorId(Long id) {
+        log.info("\nBuscando lancamento pelo ID {}\n", id);
+        return lancamentoRepository.findById(id);
+    }
+
+    @Override
+    public Lancamento persistir(Lancamento lancamento) {
+        log.info("\nPersistindo o lancamento: {}\n", lancamento);
+        return this.lancamentoRepository.save(lancamento);
+    }
+
+    @Override
+    public void remover(Long id) {
+        log.info("\nRemovendo o lancamento ID {}\n", id);
+        this.lancamentoRepository.deleteById(id);
+    }
+}

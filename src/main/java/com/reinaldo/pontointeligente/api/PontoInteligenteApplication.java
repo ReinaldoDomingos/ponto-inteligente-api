13094e2fdd6359baa16d448d588512ef0998f0f7
ReinaldoDomingos <reijunior100@gmail.com>
2020-05-26 01:32:22 -0400

    Implementação da autorização e autenticação por token JWT.

--- src/main/java/com/reinaldo/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/reinaldo/pontointeligente/api/PontoInteligenteApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //@SpringBootApplication
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PontoInteligenteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PontoInteligenteApplication.class, args);
+    }
 
 }

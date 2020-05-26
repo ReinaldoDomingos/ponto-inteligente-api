12a4a6a25314b4b3352e5bbece5979ec364bc652
ReinaldoDomingos <reijunior100@gmail.com>
2020-05-26 02:35:45 -0400

    Adicionado EhCache

--- src/main/java/com/reinaldo/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/reinaldo/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -7,6 +7,8 @@ import com.reinaldo.pontointeligente.api.services.LancamentoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -26,13 +28,13 @@ public class LancamentoServiceImpl implements LancamentoService {
         return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
     }
 
-    @Override
+    @Cacheable("lancamentoPorId")
     public Optional<Lancamento> buscarPorId(Long id) {
         log.info("\nBuscando lancamento pelo ID {}\n", id);
         return lancamentoRepository.findById(id);
     }
 
-    @Override
+    @CachePut("lancamentoPorId")
     public Lancamento persistir(Lancamento lancamento) {
         log.info("\nPersistindo o lancamento: {}\n", lancamento);
         return this.lancamentoRepository.save(lancamento);
